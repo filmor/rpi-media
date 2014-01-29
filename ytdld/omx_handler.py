d3f6f0701303a0db20e8a9c65f200e4d40bedbe1
Benedikt Sauer <filmor@gmail.com>
2014-01-29 23:49:23 +0100

    Fixed location getting.

--- ytdld/omx_handler.py
+++ ytdld/omx_handler.py
@@ -1,29 +1,31 @@
 from tornado.web import RequestHandler
 from pyomxplayer import OMXPlayer
 
+from os.path import join
+
 _current_player = None
 
 class OmxRequestHandler(RequestHandler):
+    def get_location(self):
+        return join("/media/video", self.get_argument("location"))
+
     def get(self):
         global _current_player
         action = self.get_argument("action")
-        print(action)
+        print(action, _current_player)
 
         if action == "play":
-            print(self.get_location())
+            location = self.get_location()
             if _current_player is not None:
                 _current_player.stop()
 
-            _current_player = OMXPlayer(self.get_location(), args="-p")
+            _current_player = OMXPlayer(location, args="-p")
             _current_player.play()
 
-        elif action == "pause":
-            if _current_player is not None:
-                _current_player.pause()
-        elif action == "stop":
-            if _current_player is not None:
-                _current_player.stop()
         elif action == "download":
-            print(self.get_location())
-            self.redirect(self.get_location())
+            if location.startswith("http"):
+                self.redirect(self.get_location())
+        else:
+            if _current_player is not None:
+                getattr(_current_player, action)()
 
